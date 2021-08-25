a24c68af2af76fafa7b645ad3d0de358493953fd
suvin manoj <suvin.manoj@vectorilo.com>
2021-08-25 17:53:51 +0530

    stock management

--- app/screens/StockManagements/index.tsx
+++ app/screens/StockManagements/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image, ScrollView, Switch, TouchableOpacity, View } from 'react-native';
-import { Text, Button, List, Avatar, Title, Dialog, TextInput, Chip, Card, Badge, Paragraph } from 'react-native-paper';
+import { Text, Button, List, Avatar, Title, Dialog, TextInput, Chip, Card, Badge, Paragraph, FAB } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
 
 import * as loginActions from 'app/store/actions/loginActions';
@@ -47,10 +47,126 @@ const StockManagement: React.FC = () => {
   
   const sheetRef = React.useRef(null);
   return (
-    <ScrollView>
 
-       
-   </ScrollView>   
+ <ScrollView>
+    <View style={styles.sidePadding}>
+
+    <View style={{marginTop:20}}>
+        <ScrollView
+          horizontal //scrolling left to right instead of top to bottom
+          showsHorizontalScrollIndicator={false} //hides native scrollbar
+          scrollEventThrottle={10} //how often we update the position of the indicator bar
+          pagingEnabled //scrolls from one image to the next, instead of allowing any value inbetween
+        >
+            <Card style={styles.shawd} >
+            <Avatar.Icon size={24} icon="shield" style={styles.clubsShield}/>
+            <Text style={{fontSize:15, fontWeight:'bold',paddingLeft:15}}>Shuttles</Text>
+            
+                  <Text style={{fontSize:13, color:"#000000",paddingLeft:15,opacity:.5}}>38 in stock</Text>
+            </Card>
+     <View>
+      <Card style={styles.shawd}>
+      <Avatar.Icon size={24} icon="shield" style={styles.clubsShield}/>
+          <Text style={{fontSize:15, fontWeight:'bold',paddingLeft:15}}>Rackets</Text>
+          
+          <Text style={{fontSize:13, color:"#000000",paddingLeft:15,opacity:.5}}>13 in stock</Text>
+      </Card>
+    </View>
+        <View>
+            <Card style={styles.shawd}>
+            <Avatar.Icon size={24} icon="shield" style={styles.clubsShield}/>
+                 <Text style={{fontSize:15, fontWeight:'bold',paddingLeft:15}}>Nets</Text>
+               
+                 <Text style={{fontSize:13, color:"#000000",paddingLeft:15,opacity:.5}}>3 in stock</Text>
+            </Card>
+        </View>
+    </ScrollView>
+      </View>
+      <Grid  style={{marginTop:20}} >
+     <Col size={15} style={styles.bgnumber}>
+     <Avatar.Icon size={24} icon="shield" style={styles.clubsShield}/>
+      <Text style={{color:'#fff',fontSize:10,textTransform:'uppercase',textAlign:'center',marginTop:12}}>May</Text>
+      <Paragraph style={{color:'#fff',fontSize:19,textAlign:'center',marginTop:10}}>6</Paragraph>
+      </Col>
+      <Col size={60}>
+      <Text style={{color:'#000537',fontSize:14,fontWeight:'bold'}}>7:00 - 9:00 AM</Text>
+      <Paragraph style={{color:'#000537',fontSize:12,}}>Sportspark University of East, Nor...</Paragraph>
+      <Paragraph style={{color:'#000537',fontSize:10,opacity:.5}}>Courts 1, 5</Paragraph>
+      </Col>
+      <Col size={25}>
+        <Row>
+      <Text style={{color:'#000537',fontSize:14,fontWeight:'bold'}}>+23</Text>
+      </Row>
+      <List.Icon icon="chevron-right" />
+     </Col>
+</Grid> 
+<Grid  style={{marginTop:20}} >
+     <Col size={15} style={styles.bgnumber}>
+      <Text style={{color:'#fff',fontSize:10,textTransform:'uppercase',textAlign:'center',marginTop:12}}>May</Text>
+      <Paragraph style={{color:'#fff',fontSize:19,textAlign:'center',marginTop:10}}>6</Paragraph>
+      </Col>
+      <Col size={60}>
+      <Text style={{color:'#000537',fontSize:14,fontWeight:'bold'}}>7:00 - 9:00 AM</Text>
+      <Paragraph style={{color:'#000537',fontSize:12,}}>Sportspark University of East, Nor...</Paragraph>
+      <Paragraph style={{color:'#000537',fontSize:10,opacity:.5}}>Courts 1, 5</Paragraph>
+      </Col>
+      <Col size={25}>
+        <Row>
+      <Text style={{color:'#000537',fontSize:14,fontWeight:'bold'}}>+23</Text>
+      </Row>
+      <List.Icon icon="chevron-right" />
+     </Col>
+</Grid> 
+<Grid  style={{marginTop:20}} >
+     <Col size={15} style={styles.bgnumber}>
+     <Avatar.Icon size={24} icon="shield" style={styles.clubsShield}/>
+      <Text style={{color:'#fff',fontSize:10,textTransform:'uppercase',textAlign:'center',marginTop:12}}>May</Text>
+      <Paragraph style={{color:'#fff',fontSize:19,textAlign:'center',marginTop:10}}>6</Paragraph>
+      </Col>
+      <Col size={60}>
+      <Text style={{color:'#000537',fontSize:14,fontWeight:'bold'}}>7:00 - 9:00 AM</Text>
+      <Paragraph style={{color:'#000537',fontSize:12,}}>Sportspark University of East, Nor...</Paragraph>
+      <Paragraph style={{color:'#000537',fontSize:10,opacity:.5}}>Courts 1, 5</Paragraph>
+      </Col>
+      <Col size={25}>
+        <Row>
+      <Text style={{color:'#000537',fontSize:14,fontWeight:'bold'}}>+23</Text>
+      </Row>
+      <List.Icon icon="chevron-right" />
+     </Col>
+</Grid> 
+<Grid  style={{marginTop:20}} >
+     <Col size={15} style={styles.bgnumber}>
+     <Avatar.Icon size={24} icon="shield" style={styles.clubsShield}/>
+      <Text style={{color:'#fff',fontSize:10,textTransform:'uppercase',textAlign:'center',marginTop:12}}>May</Text>
+      <Paragraph style={{color:'#fff',fontSize:19,textAlign:'center',marginTop:10}}>6</Paragraph>
+      </Col>
+      <Col size={60}>
+      <Text style={{color:'#000537',fontSize:14,fontWeight:'bold'}}>7:00 - 9:00 AM</Text>
+      <Paragraph style={{color:'#000537',fontSize:12,}}>Sportspark University of East, Nor...</Paragraph>
+      <Paragraph style={{color:'#000537',fontSize:10,opacity:.5}}>Courts 1, 5</Paragraph>
+      </Col>
+      <Col size={25}>
+        <Row>
+      <Text style={{color:'#000537',fontSize:14,fontWeight:'bold'}}>+23</Text>
+      </Row>
+      <List.Icon icon="chevron-right" />
+     </Col>
+</Grid> 
+<Grid>
+  <Col>
+<FAB
+    style={styles.fab}
+    small
+    icon="plus"
+    onPress={() => console.log('Pressed')}
+  />
+  </Col>
+  </Grid>
+      </View> 
+    </ScrollView>
+
+
    
   );
 };
--- app/screens/StockManagements/styles.ts
+++ app/screens/StockManagements/styles.ts
@@ -15,99 +15,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'transparent'
   },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  imghig:{
-    height:150,
-    width:150
-  },
-  mttext:{
-    marginTop:20,
-    fontSize:16,
-    textAlign:'left',
-    fontWeight:'bold'
-  },
-  cardtitlepad:{
-    paddingRight:30,
-    
-  },
-  bgclr:{
-    backgroundColor:'#CF3918',
-    color:'white',
-    width:'100%',
-    borderRadius:7
-
-  },
   borderClubs:{
     borderColor: "#ddd",
     borderWidth: 1,
     marginTop:20,
-    borderRadius: 5,
-    padding:8,
+    borderRadius: 9,
+    padding:10,
   },
-  clubsShield:{marginTop:13,
-    backgroundColor:'grey',
+  sidePadding:{
+    paddingHorizontal:15,
+    backgroundColor:'#fff',
+ 
   },
-  padbg:{
-   marginLeft:20,
-   marginRight:20,
-   marginTop:60
-  },
-  clubsShields:{marginTop:13,
-    backgroundColor:'orange',
-height:40,
-width:40,
-textAlign:'center',
-borderRadius: 20,
-
-  },
-  bgcont:{
-    marginTop:50,
-    backgroundColor:'#CF3918',
-    borderRadius: 5,
-    color:'white'
-  },
-  ir:{
-    marginTop: 25,
-    height:40,
-    marginLeft:20,
-    backgroundColor: "#ffffff",
-    textAlign: 'center',color: "#61dafb",
-  },
-  navBarLeftButton: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    width: 100,
-    paddingLeft: 8
-  },
-
-  buttonText: {
-  color:'black',
-    fontSize: 12,
-    position:'absolute',
-    paddingTop:100,
-   paddingLeft:12,
-   textAlign:'center',
-
-  },
-  buttonText2: {
-    color:'black',
-      fontSize: 12,
-      position:'absolute',
-      paddingTop: 125,
-      paddingLeft:10,
-      textAlign:'center',
+  shawd:{
+    width:150,  margin:10,padding:10,
+    shadowColor: '#000',
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 15,  
+    elevation: 5
     },
-    barbottom:{
-borderTopWidth:1,
-borderBottomColor:"#ccc",
-marginTop:90,
+    fab: {
+      position: 'absolute',
+      margin: 16,
+      right: 0,
+      bottom: 0,
     },
+  
  
 });
 
